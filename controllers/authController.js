f824db11c0fc61a291d00dc8fb20434e8861921b
Mohamed-dev-hany <mohamedteam64@gmail.com>
2025-06-28 21:03:54 +0300

    token

--- controllers/authController.js
+++ controllers/authController.js
@@ -103,6 +103,7 @@ exports.auth = async (req, res, next) => {
       req.headers.authorization.startsWith("Bearer")
     ) {
       token = req.headers.authorization.split(" ")[1];
+      console.log("Token:", token);
     }
     if (!token) {
       return next(new Error("You are Not Logging In"));
