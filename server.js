aaed0bd9bac2d38451fa40aeb52b1a1ef4bc1450
Mohamed-dev-hany <mohamedteam64@gmail.com>
2025-06-28 22:32:52 +0300

    review

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(helmet())
 
 app.use(cors());
 app.use(compression());
-app.use('/user', cors(), express.static('uploads/users'));
+app.use('/user', cors(), express.static(path.join(__dirname, 'uploads/user')));
 
 app.use(express.json({ limit: 2 * 1024 * 1024 }));
 app.use(express.urlencoded({ extended: true, limit: 2 * 1024 * 1024 }));
